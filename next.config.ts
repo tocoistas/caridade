a70314236be5b447f0fafa2e912229c9681662d3
seckt0r <benone.marcos@gmail.com>
2026-05-23 12:17:45 +0100

    fix: resolver depreciação node 20 adicionando env var e desativar otimização de imagens para github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: __dirname,
   },
